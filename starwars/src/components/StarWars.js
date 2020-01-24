38bc3dab52c2142c3f98f8efdf5319b3ced6b637
Gideon <reachgiddy@gmail.com>
2020-01-24 16:12:45 -0500

    Created the MVP

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
+import "./StarWars.css"
 import StarWarsCard from "./StarWarsCard";
 
 const StarWars = props => {
   console.log(props.parentStarWarsData);
-  const [charactersData] = useState(
-    props.parentStarWarsData);
+//   const [charactersData] = useState(
+//     props.parentStarWarsData);
   return (
-    <div>
-      {charactersData.map(data => {
-        return (
+    <div className="container">
+      {props.parentStarWarsData.map(data => (
+       
             <StarWarsCard 
             key={data.height} 
             character={data}/>
-        )
-      })}
+        
+  ))}
     </div>
+      
   );
 };
 
--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,19 +1,37 @@
 import React from "react";
-import { Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import "./StarWars.css"
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle
+} from "reactstrap";
 
-const StarWarsCard = (props) => {
-    console.log(props);
+const StarWarsCard = props => {
+  console.log(props);
   return (
-    <div>
-    <Card>
-    <CardBody>
-      <CardTitle><h3>name:</h3>{props.character.name}</CardTitle>
-      <CardSubtitle><p>height:</p>{props.character.height}</CardSubtitle>
-      <CardSubtitle><p>mass:</p>{props.character.mass}</CardSubtitle>
-      
-    </CardBody>
-  </Card>
-    </div>
+   
+     
+       
+          <Card id="card">
+            <CardBody id="height">
+              <CardTitle>
+                <h4>{props.character.name}</h4>
+              </CardTitle>
+              <CardSubtitle>
+                <p>height:</p>
+                {props.character.height}
+              </CardSubtitle>
+              <br></br>
+              <CardSubtitle>
+                <p>mass:</p>
+                {props.character.mass}
+              </CardSubtitle>
+            </CardBody>
+          </Card>
+        
+     
+    
   );
 };
 
